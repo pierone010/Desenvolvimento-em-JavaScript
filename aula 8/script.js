1defeefac52ad001e247a3af984eb280b1dc2371
pierone010 <andrezinho.camisoti@gmail.com>
2026-02-11 21:55:20 -0300

    Add files via upload

---
+++ aula 8/script.js
@@ -0,0 +1,49 @@
+// comentario
+console.log('4 tipos de dados basicos primitivos')
+
+//strings
+console.log('isso e um texto 1')
+console.log('isso e um texto 2')
+console.log("100000" -10)
+
+//nulo
+console.log(null)
+
+//indefinido
+console.log (undefined)
+
+//dados boleanos
+//resposta a uma condição
+console.log('saida', 10>2)
+console.log(true)
+console.log(false)
+
+//numeros inteiros
+//idade, ano, quantidade
+console.log(20)
+console.log(0)
+console.log(-1)
+console.log(0)
+
+//reais floats
+//dinheiro, altura
+console.log(10.9)
+console.log(5.9)
+console.log(25.88)
+
+
+//variaveis em js
+
+//let
+//const
+
+nome = 'carlos'
+console.log(nome)
+// alert(nome)
+const nacionalidade = 'brasileiro'
+console.log(nome,nacionalidade)
+
+let nome1='julia'
+let sobrenome='almeida'
+
+console.log(nome1,sobrenome, nacionalidade)
