2afc46272da7974f9dc71403176c6fc52f2e8567
pierone010 <andrezinho.camisoti@gmail.com>
2026-02-24 21:53:42 -0300

    Add files via upload

---
+++ aula 13/arrays.js
@@ -0,0 +1,14 @@
+let frutas = ['maçã', 'banana','kiwi']
+//              0        1        2
+frutas.push('laranja')
+ let n = [1,2,3,4]
+let x = n.map((x)) = x % 2
+console.log(frutas)
+
+// console.log(frutas.length)
+// console.log(frutas[3])
+// frutas.unshift
+// frutas.shift
+// frutas.pop()
+// console.log(frutas)
+// console.log(frutas.map())
